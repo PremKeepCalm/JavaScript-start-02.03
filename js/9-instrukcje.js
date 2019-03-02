37cc108d8bdf3fb2b97e8e478ff315394591aecb
Przemyslaw Kaczor <przem.kacz7@gmai.com>
2019-03-02 15:39:37 +0100

    9-instrukcje

---
+++ js/9-instrukcje.js
@@ -0,0 +1,34 @@
+let wynik = 10 + 10 === 20 ? 'prawda' : 10 + 10 === 20 ? 'fałsz': 'uuuu nie wiem co to jest'; //ternary
+
+// console.log(wynik);
+
+// if (2 + 2 === 4) {
+//     console.log('prawda')
+// } else  {
+//     console.log('fałsz');
+// }
+
+let kolor = 'czarny';
+
+// if (kolor === 'niebieski') {
+//         console.log('kolor')
+//     } else if (kolor === 'fioletowy') {
+//         console.log('czarny')
+    
+        
+//     }
+//         console.log('fałsz');
+//     }
+
+
+switch (kolor) {
+    case 'niebieski':
+    console.log(kolor);
+    break;
+    case 'fioletowy':
+    console.log(kolor);
+    break;
+    case 'czarny':
+    console.log(kolor);
+    break;
+}
